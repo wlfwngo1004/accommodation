5718d9fbd74cfcb7126afe7cb8a6fe92ee4d47c8
Seungki <130020647+Seungkizz@users.noreply.github.com>
2023-10-23 14:56:11 +0900

    MemberDto 생성
    값 받아지는지 TEST

---
+++ src/main/java/com/ourproject/accommodation/dto/MemberDto.java
@@ -0,0 +1,19 @@
+package com.ourproject.accommodation.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class MemberDto {
+
+    private String m_id;
+    private String m_name;
+    private String m_pwd;
+    private String m_phone;
+    private String m_mail;
+    private String m_addr;
+    private Date m_reg_date;
+    private int m_lev;
+
+}

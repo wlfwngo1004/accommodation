80a683d9a4bac3a420242773624d39e8305ecea3
Seungki <130020647+Seungkizz@users.noreply.github.com>
2023-10-24 20:16:38 +0900

    토큰 생성 & 정보 GET

--- src/main/java/com/ourproject/accommodation/member/controller/KakaoOauthController.java
+++ src/main/java/com/ourproject/accommodation/member/controller/KakaoOauthController.java
@@ -1,5 +1,7 @@
 package com.ourproject.accommodation.member.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ourproject.accommodation.member.dto.SocialUserInfoDto;
 import com.ourproject.accommodation.member.service.KakaoOauthService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +23,15 @@ public class KakaoOauthController {
     }
 
     @GetMapping("/kakaoLogin")
-    public String kakaoLogin(@RequestParam("code") String code)  {
+    public String kakaoLogin(@RequestParam("code") String code) throws JsonProcessingException {
 
         // 1. "인가 코드"로 "액세스 토큰" 요청
         log.info("code = " + code);
         String accessToken = kakaoOauthService.getAccessToken(code);
         // 2. 토큰으로 카카오 API 호출
         log.info("accessToken = " + accessToken);
-        //SocialUserInfoDto kakaoUserInfo = getKakaoUserInfo(accessToken);
+        SocialUserInfoDto kakaoUserInfo = kakaoOauthService.getKakaoUserInfo(accessToken);
+        log.info("kakaoUserInfo = " + kakaoUserInfo);
         return "";
     }
 
--- src/main/java/com/ourproject/accommodation/member/service/KakaoOauthService.java
+++ src/main/java/com/ourproject/accommodation/member/service/KakaoOauthService.java
@@ -1,7 +1,13 @@
 package com.ourproject.accommodation.member.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ourproject.accommodation.member.dto.SocialUserInfoDto;
+
 public interface KakaoOauthService {
 
+    // "인가 코드"로 "액세스 토큰" 요청
+    String getAccessToken(String code) throws JsonProcessingException;
 
-    String getAccessToken(String code);
+    // 토큰으로 카카오 API 호출
+    SocialUserInfoDto getKakaoUserInfo(String accessToken) throws JsonProcessingException;
 }
